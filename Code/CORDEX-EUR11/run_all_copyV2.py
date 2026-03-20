d7d8e03d00aa21fc868b5c9492752f27dbdea241
loumdt <mandonnet@tutanota.com>
2026-03-20 17:31:22 +0100

    update

--- Code/CORDEX-EUR11/run_all_copyV2.py
+++ Code/CORDEX-EUR11/run_all_copyV2.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     start_time = time.time()
     # Collect details and find read folder of temperature variable
     #combination = tuple((sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]))
+    combination = tuple(('CNRM-CERFACS-CNRM-CM5','GERICS-REMO2015','rcp85','v2'))
     read_directory_historical = "/home/user/These/cordex_htws_cc3d/Data/historical"#sys.argv[5]
     read_directory_rcp = "/home/user/These/cordex_htws_cc3d/Data/rcp85"#sys.argv[6]
 
@@ -19,12 +20,12 @@ if __name__ == "__main__":
     start_year_ref = 1971
     end_year_ref = 2025
     temp_variable = 'tas' # Will need to change to tasmax
-    smooth_span = 15
     threshold_value = 95
     distrib_window_size = 15
-    anomaly = False
+    anomaly = True
     relative_threshold = True
     nb_days = 4
+    connectivity = 26
     #%%
     #read_directory_historical = "/bdd/CORDEX/output/EUR-11/GERICS/CNRM-CERFACS-CNRM-CM5/historical/r1i1p1/GERICS-REMO2015/v2/day/tas/latest"
     #read_directory_rcp = "/bdd/CORDEX/output/EUR-11/GERICS/CNRM-CERFACS-CNRM-CM5/rcp85/r1i1p1/GERICS-REMO2015/v2/day/tas/latest"
@@ -44,12 +45,12 @@ if __name__ == "__main__":
     #%% Compute climatology smooth
     print("--- %.2f seconds ---" % (time.time() - start_time))
     print("Running compute_climatology_smooth...")
-    compute_climatology_smooth(read_directory_historical,read_directory_rcp,write_directory,start_year_ref=start_year_ref,end_year_ref=end_year_ref,temp_variable=temp_variable,smooth_span=smooth_span)
+    #compute_seasonal_cycle(read_directory_historical,read_directory_rcp,write_directory,start_year_ref=start_year_ref,end_year_ref=end_year_ref,temp_variable=temp_variable)
     print("Done.")
     print("--- %.2f seconds ---" % (time.time() - start_time))
     #%% Compute distribution of reference period and threshold
     print("Running compute_distrib_percentile...")
-    compute_distrib_percentile(read_directory_historical,read_directory_rcp,write_directory,start_year_ref=start_year_ref,end_year_ref=end_year_ref,temp_variable=temp_variable,threshold_value=threshold_value,distrib_window_size=distrib_window_size,anomaly=anomaly)
+    #compute_distrib_percentile(read_directory_historical,read_directory_rcp,write_directory,start_year_ref=start_year_ref,end_year_ref=end_year_ref,temp_variable=temp_variable,threshold_value=threshold_value,distrib_window_size=distrib_window_size,anomaly=anomaly)
     print("Done.")
     print("--- %.2f seconds ---" % (time.time() - start_time))
     #%% Compute heatwaves CC3D scan
@@ -60,5 +61,10 @@ if __name__ == "__main__":
     #%% Compute Regional Warming Level periods
     print("Running compute_regional_warming_levels ...")
     compute_regional_warming_levels(read_directory_historical,read_directory_rcp,write_directory,start_year=start_year,end_year=end_year,start_year_ref=1986,end_year_ref=2005,ref_period_offset=0.72,running_mean_window_size=20,regional_warming_levels_list=[2.1,2.6,4.0,5.1])
+    print("Done.")
+    print("--- %.2f seconds ---" % (time.time() - start_time))
+    #%% Compute HWMId index
+    print("Running compute_Russo_HWMId ...")
+    compute_Russo_HWMId(read_directory_historical,read_directory_rcp,write_directory,start_year=start_year,end_year=end_year,start_year_ref=start_year_ref,end_year_ref=end_year_ref,temp_variable=temp_variable)
     print("Done.")
     print("--- %.2f seconds ---" % (time.time() - start_time))
