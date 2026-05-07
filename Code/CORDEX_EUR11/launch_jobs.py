28daabf18da5805622090ee1b94b9cdfa509248a
loumdt <mandonnet@tutanota.com>
2026-05-07 14:32:53 +0200

    add bias correction

--- Code/CORDEX_EUR11/launch_jobs.py
+++ Code/CORDEX_EUR11/launch_jobs.py
@@ -10,7 +10,9 @@ def json_loads_dictionary(json_array_having_key_and_value_as_keys):
         dict_having_tuple_as_key[ tuple(dict_having_key_and_value_as_keys['key']) ] = dict_having_key_and_value_as_keys['value']
     return dict_having_tuple_as_key
 
-with open('/home/tmandonnet/CORDEX/CORDEX_pairs_path_dict.json', 'r') as f:
+choice = 'CORDEX_ADJUST' #'CORDEX' or 'CORDEX_ADJUST'
+
+with open(f'/home/tmandonnet/CORDEX/{choice}_pairs_path_dict.json', 'r') as f:
     data = json.load(f)
 
 loaded_dict = json_loads_dictionary(data)
